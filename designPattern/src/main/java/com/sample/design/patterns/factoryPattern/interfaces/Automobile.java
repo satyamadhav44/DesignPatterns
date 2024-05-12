af4df31f9a9b3f4993d9d53c5bd51adbe0edc526
Satya Theegela <satyamadhav44@gmail.com>
2024-05-12 21:02:06 +0800

    maven code refoctor

--- designPattern/src/main/java/com/sample/design/patterns/factoryPattern/interfaces/Automobile.java
+++ designPattern/src/main/java/com/sample/design/patterns/factoryPattern/interfaces/Automobile.java
@@ -1,4 +1,4 @@
-package com.sample.design.patterns.factoryPattern.interfaces.;
+package com.sample.design.patterns.factoryPattern.interfaces;
 
 public interface Automobile {
     public void fuelType();
